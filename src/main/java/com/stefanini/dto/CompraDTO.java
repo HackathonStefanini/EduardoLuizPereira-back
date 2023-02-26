2ce805003eb8edb1cf3927f8d3c4c944904b50f9
Eduardo Luiz <eduardoluizsp1@gmail.com>
2023-02-26 17:57:39 -0300

    Implementando parte de calcular preco e comprar

---
+++ src/main/java/com/stefanini/dto/CompraDTO.java
@@ -0,0 +1,23 @@
+package com.stefanini.dto;
+
+public class CompraDTO {
+    private Long idJogador;
+    private Long idStefamon;
+
+    // Getters e Setters
+    public Long getIdJogador() {
+        return idJogador;
+    }
+
+    public void setIdJogador(Long idJogador) {
+        this.idJogador = idJogador;
+    }
+
+    public Long getIdStefamon() {
+        return idStefamon;
+    }
+
+    public void setIdStefamon(Long idStefamon) {
+        this.idStefamon = idStefamon;
+    }
+}
